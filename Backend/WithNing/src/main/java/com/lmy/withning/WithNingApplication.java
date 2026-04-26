d82cf2ec0ca211e3e4e462d78a0d9950563a0af4
Liumy1212 <Liumyhx@163.com>
2026-04-26 17:14:44 +0800

    first commit

---
+++ Backend/WithNing/src/main/java/com/lmy/withning/WithNingApplication.java
@@ -0,0 +1,14 @@
+package com.lmy.withning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class WithNingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithNingApplication.class, args);
+    }
+
+}
---
+++ Backend/WithNing/src/test/java/com/lmy/withning/WithNingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmy.withning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithNingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
